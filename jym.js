0e74ecd2bdf43fd94c0ad66ed6a90cc0b115e97a
juyoonmin <goaxhfl0109@naver.com>
2022-11-24 12:58:58 +0900

    스와이퍼 2가지 설정모드 반응형처리 리사이즈 미처리

--- jym.js
+++ jym.js
@@ -1,25 +1,59 @@
-window.onload = function(){
-    const swiper_mbanner = new Swiper('#banner.swiper', {
-        slidesPerView: 3,
-        spaceBetween: 0,
-        slidesPerGroup: 4,
-        centeredSlides: true,
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
-          pagination: {
-          el: '.swiper-pagination',
-        },
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
-      });
-}
+  $(document).ready(function(){
+
+    var _width = $(window).width();
+  
+    const loop_auto_center_3_pc ={      
+      loop: true,    
+      containerModifierClass : 'pc_slide-',     
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction : false,
+      },
+      slidesPerView: 3,
+      spaceBetween: 0,
+      centeredSlides: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+  
+    const loop_auto_center_3_m ={      
+      loop: true,       
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction : false,
+      },
+      slidesPerView: 1.5,
+      spaceBetween: 20,  
+      pagination: {
+        el: '.swiper-pagination',
+        type: "fraction",
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },   
+    }
+  
+  
+    var swiper_mbanner = undefined; 
+  
+    if( _width < 768 ){     
+      if(swiper_mbanner != undefined){ 
+         swiper_mbanner.destroy(); 
+      }
+      swiper_mbanner = new Swiper('#banner.swiper', loop_auto_center_3_m );    
+    }else{   
+      if(swiper_mbanner != undefined){ 
+        swiper_mbanner.destroy(); 
+     }
+     swiper_mbanner = new Swiper('#banner.swiper', loop_auto_center_3_pc ); 
+    }
+  
+  })
+  
+     
+  
